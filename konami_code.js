3d6671a52fe3a9c2850df3d29b1c02ac072c1064
Anthony C <acherone92@gmail.com>
2018-04-17 13:10:20 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,16 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-  document.body.addEventListener()
+  document.body.addEventListener('keydown',onKeyDownHandler());
 let index = 0;
 
 function onKeyDownHandler(e){
   console.log(`Detail: ${e.detail}\nWhich: ${e.which}\nLocation: ${e.location}`);
-  const key = parseInt(e.detail)
+  const key = parseInt(e.detail);
     if (key==code[index]){
       index++;
       if (index ==code.length){
-        alert()
+        alert("")
       }
   }
 }
